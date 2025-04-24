d230916a4177fe2f996156024cbebece922cc7a5
Biswamohan singh <prasannasingh50@gmail.com>
2025-04-24 20:46:41 +0530

    Create 383. Ransom_Note.c

---
+++ Counting/383. Ransom_Note.c
@@ -0,0 +1,16 @@
+bool canConstruct(char* ransomNote, char* magazine) {
+    
+    int counter[26] = {0};
+
+    for (int i = 0; magazine[i] != '\0'; i++) {
+        counter[magazine[i] - 'a']++;
+    }
+
+    for (int i = 0; ransomNote[i] != '\0'; i++) {
+        if (--counter[ransomNote[i] - 'a'] < 0) {
+            return false;
+        }
+    }
+
+    return true;
+}

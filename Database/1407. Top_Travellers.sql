2b6833f65ddd8ff7300151ea9ff7d95681e9d031
Biswamohan singh <prasannasingh50@gmail.com>
2025-04-28 20:45:45 +0530

    Create 1407. Top_Travellers.sql

---
+++ Database/1407. Top_Travellers.sql
@@ -0,0 +1,5 @@
+select name, 
+(select case 
+when sum(distance) is not null then sum(distance) else 0 end 
+from Rides R where R.user_id = U.id) as travelled_distance 
+from Users U order by travelled_distance desc, name asc
